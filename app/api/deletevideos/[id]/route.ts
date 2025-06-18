c6953b65497e715addcbe3bb5d72c6407ffe1c91
MUHAMMAD ISMAIL <150179291+Mismail9961@users.noreply.github.com>
2025-06-18 21:02:09 +0500

    final test

--- app/api/deletevideos/[id]/route.ts
+++ app/api/deletevideos/[id]/route.ts
@@ -1,10 +1,10 @@
-import { NextResponse } from "next/server";
+import { NextRequest,NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 export async function DELETE(
-  request: Request,
+  request: NextRequest,
   context: { params: { id: string } }
 ) {
   const { id } = context.params;
